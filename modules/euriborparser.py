90590aefce0e7a6e83d753e94a3fa043376ae632
DoeWo <doedlingerwolfgang@gmail.com>
2023-08-13 21:34:32 +0200

    added time logic to euribor parser

--- modules/euriborparser.py
+++ modules/euriborparser.py
@@ -12,6 +12,7 @@ class EuriborParser():
     URL_HIST = 'https://www.euribor-rates.eu/en/euribor-rates-by-year'
     URL_CURR = 'https://www.euribor-rates.eu/en/current-euribor-rates/2/euribor-rate-3-months/'
     YEARS = list(range(1999,dt.datetime.today().year + 1))
+    HEUTE = dt.datetime.today()
 
     def __init__(self, current_euribor=None):
         self.current_euribor = pd.DataFrame(columns=["3M_EURIBOR"], index=pd.DatetimeIndex([]))
@@ -26,27 +27,43 @@ class EuriborParser():
         This function gets the most recent EURIBOR value
         """
         try:
+            if EuriborParser.HEUTE.weekday() in (0,1,2,3,4):
+                check_tag = EuriborParser.HEUTE
+            elif EuriborParser.HEUTE.weekday() in (5,6):
+                check_tag = EuriborParser.HEUTE - dt.timedelta(days=EuriborParser.HEUTE.weekday()%4)
+
             self.current_euribor = pd.read_csv(r"./data/current_euribor.csv", index_col=0)
             self.current_euribor.index = pd.to_datetime(self.current_euribor.index)
             self.current_euribor.index.freq = "B"
+            self.current_euribor.loc[dt.datetime.strftime(check_tag, "%Y-%m-%d")]
             print("read from csv")
+
         except (KeyError, FileNotFoundError):
-            request = r.get(EuriborParser.URL_CURR)
-            soup = BeautifulSoup(request.content, 'lxml')
-            current_table = soup.find('div', class_="col-12 col-lg-4 mb-3 mb-lg-0")
-            first_row = current_table.find('tbody').find('tr')
-            date = first_row.find_all('td')[0].text
-            percentage = first_row.find_all('td')[1].text.strip().rstrip(" %")
-            date = dt.datetime.strptime(date, "%m/%d/%Y")
-            self.current_euribor.loc[date] = percentage
-            self.current_euribor.index.freq = "B"         
-            self.current_euribor.to_csv(r"./data/current_euribor.csv")
-            print("read from Homepage")
+
+            if dt.datetime.now().time() < dt.datetime.strptime("16:00", "%H:%M"):
+                self.current_euribor = pd.read_csv(r"./data/current_euribor.csv", index_col=0)
+                self.current_euribor.index = pd.to_datetime(self.current_euribor.index)
+                self.current_euribor.index.freq = "B"
+                self.current_euribor.loc[dt.datetime.strftime(check_tag, "%Y-%m-%d")]
+                print("read from csv")                
+            else:
+                request = r.get(EuriborParser.URL_CURR)
+                soup = BeautifulSoup(request.content, 'lxml')
+                current_table = soup.find('div', class_="col-12 col-lg-4 mb-3 mb-lg-0")
+                first_row = current_table.find('tbody').find('tr')
+                date = first_row.find_all('td')[0].text
+                percentage = first_row.find_all('td')[1].text.strip().rstrip(" %")
+                date = dt.datetime.strptime(date, "%m/%d/%Y")
+                self.current_euribor.loc[date] = percentage
+                self.current_euribor.index.freq = "B"         
+                self.current_euribor.to_csv(r"./data/current_euribor.csv", mode="a")
+                print("read from Homepage")
+
         return self.current_euribor
 
 
 if __name__ == "__main__":
     test = EuriborParser()
     current = test.parse_current()
-    print(current[0])
+    print(current)
     print(current.index)
